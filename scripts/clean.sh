18e9c1724534e9286f86e516687e02e50b760808
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-07-21 00:02:26 -0400

    chore: clean executable generated by run script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,9 +4,11 @@
 if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "darwin"* ]]; then
   rm ./data/*.mp4
   rm ./log/*-*.log
+  rm main
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
   del .\data\*.mp4
   del .\log\*.log
+  del main
 else
   echo "Unknown operating system"
 fi
